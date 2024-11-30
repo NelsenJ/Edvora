e081eab11721d1cca14f02a2097096aced43acb3
NelsenJ <nelsen.johnathan@gmail.com>
2024-11-30 19:15:30 +0700

    adding Dashboard page

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Button from '../components/Button';
+
+const Dashboard = () => {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+};
+
+export default Dashboard;
